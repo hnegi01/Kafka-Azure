304fc9eadcd66304e48cda7538d7761e4b44c6f2
hnegi01 <46898338+hnegi01@users.noreply.github.com>
2020-12-24 19:28:22 -0500

    Update kafka-adl.py

--- kafka-adl.py
+++ kafka-adl.py
@@ -1,6 +1,7 @@
 from kafka import KafkaConsumer
 import json
 import pandas as pd
+from io import StringIO,BytesIO
 from datetime import datetime,timedelta
 import pyarrow as pa
 import pyarrow.parquet as pq
@@ -21,18 +22,24 @@ consumer = KafkaConsumer(
     group_id='Consumer Group ID',
     value_deserializer=lambda m:json.loads(m.decode('utf-8')))
 
-print("starting the consumer")
 
+print("starting the consumer")
+i = 0
 l1 = []
 for msg in consumer:
     test = msg.value
     l1.append(test)
-    df = pd.DataFrame(l1)
-    table = pa.Table.from_pandas(df, preserve_index=False)
-    pq.write_table(table, 'data.parquet')
-    blob_client = blob_service_client.get_container_client(container=container_name)
-    current_date = datetime.now()
-    blob = blob_client.upload_blob('customers.parquet',data='data.parquet', overwrite=True)
-    print('Uploading')
+    i = i + 1
+    if i >= 6000:
+        df = pd.DataFrame(l1)
+        f = BytesIO()
+        df.to_parquet(f)
+        f.seek(0)
+        blob_client = blob_service_client.get_container_client(container=container_name)
+        current_date = datetime.now()
+        blob_client.upload_blob('customer', data=f, overwrite=True)
+        i = 0
+        l1 = []
+        print('Uploading')
 
 
